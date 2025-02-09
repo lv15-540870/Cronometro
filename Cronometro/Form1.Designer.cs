add01582f870ea71339653fdc8f838c5ff98c031
lv15-540870 <luisantoniocuevasortizpvc@gmail.com>
2025-02-09 10:10:55 -0600

    Agregar archivos de proyecto.

---
+++ Cronometro/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Cronometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCronometro = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.timerCronometro = new System.Windows.Forms.Timer(this.components);
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnResetear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCronometro
+            // 
+            this.lblCronometro.AutoSize = true;
+            this.lblCronometro.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCronometro.Location = new System.Drawing.Point(48, 50);
+            this.lblCronometro.Name = "lblCronometro";
+            this.lblCronometro.Size = new System.Drawing.Size(368, 95);
+            this.lblCronometro.TabIndex = 0;
+            this.lblCronometro.Text = "00:00:00";
+            this.lblCronometro.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Red;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCerrar.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Location = new System.Drawing.Point(569, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 30);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "OFF";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // timerCronometro
+            // 
+            this.timerCronometro.Interval = 1000;
+            this.timerCronometro.Tick += new System.EventHandler(this.timerCronometro_Tick);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.DimGray;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIniciar.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(486, 50);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(114, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.DimGray;
+            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDetener.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(486, 79);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(114, 23);
+            this.btnDetener.TabIndex = 3;
+            this.btnDetener.Text = "DETENER";
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnResetear
+            // 
+            this.btnResetear.BackColor = System.Drawing.Color.DimGray;
+            this.btnResetear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnResetear.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResetear.Location = new System.Drawing.Point(486, 108);
+            this.btnResetear.Name = "btnResetear";
+            this.btnResetear.Size = new System.Drawing.Size(114, 23);
+            this.btnResetear.TabIndex = 4;
+            this.btnResetear.Text = "RESETEAR";
+            this.btnResetear.UseVisualStyleBackColor = false;
+            this.btnResetear.Click += new System.EventHandler(this.btnResetear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(643, 168);
+            this.Controls.Add(this.btnResetear);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.lblCronometro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCronometro;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Timer timerCronometro;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnResetear;
+    }
+}
+
---
+++ Cronometro/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Horas = 0;
+        int Min = 0;
+        int Seg = 0;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerCronometro.Enabled = true;
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            timerCronometro.Enabled = false;
+        }
+
+        private void btnResetear_Click(object sender, EventArgs e)
+        {
+            timerCronometro.Enabled = false;
+            lblCronometro.Text = "00:00:00";
+        }
+
+        private void timerCronometro_Tick(object sender, EventArgs e)
+        {
+            Seg++;
+            if (Seg >= 60)
+            {
+                Seg = 0;
+                Min++;
+
+                if (Min >= 60) 
+                {
+                    Min = 0;
+                    Horas++;
+                }
+            }
+            DateTime cronometro = new DateTime(2025, 1, 1, Horas, Min, Seg);
+            lblCronometro.Text = cronometro.ToString("HH:mm:ss");
+        }
+    }
+}
